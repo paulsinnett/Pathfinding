6356d00a5885055c22ee23e7c7d5503251eb6410
Paul Sinnett <paul.sinnett@gmail.com>
2015-04-21 15:57:23 +0100

    Example project

---
+++ Assets/UnityVS/AI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SetTarget(Vector3 position)
+	{
+		agent.SetDestination (position);
+	}
+}
---
+++ Assets/UnityVS/Pathfinding.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pathfinding : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown () {
+	
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		//Debug.DrawRay (ray.origin, ray.direction * 100f, Color.red, 1f);
+
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit)) {
+
+			Debug.DrawRay (hit.point, Vector3.up, Color.red, 1f);
+
+			foreach (AI ai in FindObjectsOfType<AI>())
+			{
+				ai.SendMessage ("SetTarget", hit.point);
+			}
+		}
+	}
+}
